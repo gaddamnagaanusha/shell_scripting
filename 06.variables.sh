c919c136c699e0f7ddf6174bc196344532be60ac
Balaji <balaji@eviridis.com>
2026-03-10 09:36:25 +0530

    notes updated

--- 06.variables.sh
+++ 06.variables.sh
@@ -2,4 +2,11 @@
  
 start_time=$(date)    # This will store the current date and time in the variable start_time
 
+sleep 5
+
+end_time=$(date)    # This will store the current date and time in the variable end_time
+
+total_time=$((end_time - start_time))
 echo "script started at $start_time"
+echo "script ended at $end_time"
+echo "total time taken: $total_time seconds"
