c6a80ccc0914cbf4587ae27c6866cba5fcc666b5
Balaji <balaji@eviridis.com>
2026-03-10 09:15:14 +0530

    notes updated

---
+++ conversions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is the comment, this will not be executed by bash. This is only for information purpose
+
+echo "suresh: hello ravi, how are you doing?"
+echo "ravi: I am doing good, how about you?"
+echo "suresh: I am doing good too, thanks for asking"
+echo "ravi: I am learning git and it is really interesting"
+echo "suresh: That's great, I am also learning git and it is really useful"
+
