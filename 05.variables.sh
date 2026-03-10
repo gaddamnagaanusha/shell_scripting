1eb4aeda3ed017bc1870eba3c1634021d12806dc
Balaji <balaji@eviridis.com>
2026-03-10 09:30:05 +0530

    notes updated

--- 05.variables.sh
+++ 05.variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "please enter your name\n"
+echo "please enter your name"
 read name
 
-echo " please enter your password"
-read -s password
+echo "please enter your password"
+read password
 
-echo " your name is $name and your password is $password"
+echo "your name is $name and your password is $password"
