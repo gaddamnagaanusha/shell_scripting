962809831e497826e3ed1a8ac294b0a854d8c31b
Balaji <balaji@eviridis.com>
2026-03-10 09:19:26 +0530

    notes updated

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$person1 ="suresh"
+$person2 ="ravi"
+
+echo "$person1: hello $person2, how are you doing?"
+echo "$person2: I am doing good, how about you?"
+echo "$person1: I am doing good too, thanks for asking"
+echo "$person2: I am learning git and it is really interesting"
+echo "$person1: That's great, I am also learning git and it is really useful"
