f1c444e46575dae05cad2eefc505858dff3c2fc4
Balaji <balaji@eviridis.com>
2026-04-23 10:07:50 +0530

    notes updated

---
+++ colours.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Define color variables
+R="\e[31m"  # Red
+G="\e[32m"  # Green
+Y="\e[33m"  # Yellow
+B="\e[34m"  # Blue
+M="\e[35m"  # Magenta
+C="\e[36m"  # Cyan
+W="\e[37m"  # White
+N="\e[0m"   # No Color (reset)]
+
+echo -e "$R Hello, i'm learning shell scripting! $N $M this is my first day of learning $N"
