476f899e482ebbb9797475cf4f9bcd49235cc577
Balaji <balaji@eviridis.com>
2026-04-23 10:48:26 +0530

    notes updated

--- 17.trap.sh
+++ 17.trap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e #ERR
-
+set -e #ERR #set used to exit the script when there is an error in any command. It will not execute the next command if there is an error in any command. It will exit the script immediately when there is an error in any command.
 trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
 echo "Hello World"
